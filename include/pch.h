79d57e8b2c4af4b67cea72270b5fcd8835055d9c
Timur <timurkashapov15@gmail.com>
2024-02-01 00:41:15 +0300

    update pch

--- include/pch.h
+++ include/pch.h
@@ -2,6 +2,12 @@
 #define PCH_H
 
 #include <iostream>
+#include <string>
+#include <functional>
+#include <memory>
 #include "boost/asio.hpp"
+#include "boost/bind/bind.hpp"
+#include "boost/shared_ptr.hpp"
+#include "boost/enable_shared_from_this.hpp"
 
 #endif // PCH_H
