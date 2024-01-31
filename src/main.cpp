b0596cb74484e9228129ddcafac63d2806ea09cd
Timur <timurkashapov15@gmail.com>
2024-02-01 00:41:55 +0300

    add try except in main cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,29 @@
-#include <iostream>
-#include "acceptor.h"
+#include <csignal>
+#include "server.h"
 
-int main(int argc, char* argv[])
-{
-    std::cout << "Hello world!" << std::endl;
+const short PORT = 1234;
+
+void signalHandler(int signum) {
+    std::cout << "Shutdown server" << std::endl;
+    exit(signum);
+}
+
+int main(int argc, char* argv[]) {
+    setlocale(LC_ALL, "RU");
+    try
+    {
+        boost::asio::io_context ioContext;
+        Server server(ioContext, PORT);
+        server.run();
+
+        std::signal(SIGINT, signalHandler);
+
+        ioContext.run();
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << "Error during running server" << std::endl;
+    }
+    
+    return 0;
 }
